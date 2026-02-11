321b61dfa1a525387606a43af92f2cf1f97ed548
chiscookeke11 <chiscookeke11@gmail.com>
2026-02-11 16:48:16 +0100

    fixed deployment errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar.jsx";
 import Hero from "./Components/Hero.jsx";
-import AboutUs from "./Components/about.jsx";
+import AboutUs from "./Components/About.jsx";
 import Service from "./Components/Service.jsx";
 import ContactPage from "./Components/ContactUs.jsx";
 import BookingPage from "./Components/BookingPage.jsx";
